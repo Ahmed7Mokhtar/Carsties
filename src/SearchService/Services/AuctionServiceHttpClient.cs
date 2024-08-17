286c83c86ab6f66422dae3b38ebf19bb1aa4050e
Ahmed Mokhtar <rooneya250@gmail.com>
2024-08-17 16:29:01 +0300

    comment

--- src/SearchService/Services/AuctionServiceHttpClient.cs
+++ src/SearchService/Services/AuctionServiceHttpClient.cs
@@ -17,11 +17,13 @@ public class AuctionServiceHttpClient
 
     public async Task<List<Item>> GetItemsForSearchDB()
     {
+        // get the latest updatedAt date in items (searchDB)
         var lastUpdated = await DB.Find<Item, string>()
             .Sort(m => m.Descending(a => a.UpdatedAt))
             .Project(m => m.UpdatedAt.ToString())
             .ExecuteFirstAsync();
 
+        // make a request to the auction service to bring auctions after the last updatedAt date (for seeding) (auctionsDB)
         return await _httpClient.GetFromJsonAsync<List<Item>>($"{_config["AuctionServiceUrl"]}/api/auctions?date={lastUpdated}");
     }
 }
